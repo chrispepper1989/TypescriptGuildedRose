5b3441eef95068af2e49ac59ec605fa6aaffc10f
Christopher Pepper <christopher.pepper@codecomputerlove.com>
2024-03-23 16:27:01 +0000

    Init: getting program to compile and run

--- test/jest/approvals.spec.ts
+++ test/jest/approvals.spec.ts
@@ -12,9 +12,31 @@ import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose Approval', () => {
 
- 
+  let gameConsoleOutput: string;
+  let originalConsoleLog: (message: any) => void;
+  let originalProcessArgv: string[]
 
- 
+  function gameConsoleLog(msg: string) {
+    if (msg) {
+      gameConsoleOutput += msg;
+    }
+    gameConsoleOutput += "\n";
+  }
+
+  beforeEach(() => {
+    // prepare capturing console.log to our own gameConsoleLog.
+    gameConsoleOutput = "";
+    originalConsoleLog = console.log;
+    console.log = gameConsoleLog;
+    originalProcessArgv = process.argv;
+  });
+
+  afterEach(() => {
+    // reset original console.log
+    console.log = originalConsoleLog;
+    process.argv = originalProcessArgv;
+  });
+
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
@@ -22,6 +44,11 @@ describe('Gilded Rose Approval', () => {
     expect(items).toMatchSnapshot();
   });
 
-
+  it('should thirtyDays', () => {
+    process.argv = ["<node>", "<script", "30"];
+    require('../golden-master-text-test.ts');
+       
+    expect(gameConsoleOutput).toMatchSnapshot();
+  });
 
 });
