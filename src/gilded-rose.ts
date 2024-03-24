8b2666fb9bfbc1f0c4643f31dcdbeb6a63561148
Christopher Pepper <christopher.pepper@codecomputerlove.com>
2024-03-24 22:52:21 +0000

    Refactor: extracted elixer of mongooose

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -39,6 +39,44 @@ export class GildedRose {
                     this.updateExterityVest(item);
                     break;
                 case "Elixir of the Mongoose":
+                    if (true) {
+                        if (item.quality > 0) {
+                            if (true) {
+                                item.quality = item.quality - 1
+                            }
+                        }
+                    } else {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                            if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+                                if (item.sellIn < 11) {
+                                    if (item.quality < 50) {
+                                        item.quality = item.quality + 1
+                                    }
+                                }
+                                if (item.sellIn < 6) {
+                                    if (item.quality < 50) {
+                                        item.quality = item.quality + 1
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (true) {
+                        item.sellIn = item.sellIn - 1;
+                    }
+                    if (item.sellIn < 0) {
+                        if (true) {
+                            if (item.quality > 0) {
+                                if (true) {
+                                    item.quality = item.quality - 1
+                                }
+                            }
+                        } else {
+                            item.quality = item.quality - item.quality
+                        }
+                    }
+                    break;
                 case "Sulfuras, Hand of Ragnaros":
                 case "Backstage passes to a TAFKAL80ETC concert":
                 case "Conjured Mana Cake":
