addd31451ba707f3a1b8b0a8547ea2a9bcabd5a7
Christopher Pepper <christopher.pepper@codecomputerlove.com>
2024-03-24 23:01:21 +0000

    Refactor: simplfying dead code paths

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -42,41 +42,19 @@ export class GildedRose {
                     break;
              
                 case "Sulfuras, Hand of Ragnaros":
-                    if (true) {
-                        if (item.quality > 0) {
-                            if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                                item.quality = item.quality - 1
-                            }
-                        }
-                    } else {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                            if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-                                if (item.sellIn < 11) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1
-                                    }
-                                }
-                                if (item.sellIn < 6) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1
-                                    }
-                                }
-                            }
+                    if (item.quality > 0) {
+                        if (item.name != 'Sulfuras, Hand of Ragnaros') {
+                            item.quality = item.quality - 1
                         }
                     }
                     if (item.name != 'Sulfuras, Hand of Ragnaros') {
                         item.sellIn = item.sellIn - 1;
                     }
                     if (item.sellIn < 0) {
-                        if (true) {
-                            if (item.quality > 0) {
-                                if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                                    item.quality = item.quality - 1
-                                }
+                        if (item.quality > 0) {
+                            if (item.name != 'Sulfuras, Hand of Ragnaros') {
+                                item.quality = item.quality - 1
                             }
-                        } else {
-                            item.quality = item.quality - item.quality
                         }
                     }
                     break;
@@ -104,9 +82,7 @@ export class GildedRose {
                             }
                         }
                     }
-                    if (true) {
-                        item.sellIn = item.sellIn - 1;
-                    }
+                    item.sellIn = item.sellIn - 1;
                     if (item.sellIn < 0) {
                         if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
                             if (item.quality > 0) {
