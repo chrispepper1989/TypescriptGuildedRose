7b025b8e43378c112434ced8ca49a8249ceeaf38
Christopher Pepper <christopher.pepper@codecomputerlove.com>
2024-03-24 23:04:37 +0000

    Refactor: Removing dead code paths

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -42,57 +42,27 @@ export class GildedRose {
                     break;
              
                 case "Sulfuras, Hand of Ragnaros":
-                    if (item.quality > 0) {
-                        if (false) {
-                            item.quality = item.quality - 1
-                        }
-                    }
-                    if (false) {
-                        item.sellIn = item.sellIn - 1;
-                    }
-                    if (item.sellIn < 0) {
-                        if (item.quality > 0) {
-                            if (false) {
-                                item.quality = item.quality - 1
-                            }
-                        }
-                    }
+                  
+                    
+                    
                     break;
                 case "Backstage passes to a TAFKAL80ETC concert":
-                    if (false) {
-                        if (item.quality > 0) {
-                            if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                                item.quality = item.quality - 1
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1
                             }
                         }
-                    } else {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                            if (true) {
-                                if (item.sellIn < 11) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1
-                                    }
-                                }
-                                if (item.sellIn < 6) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1
-                                    }
-                                }
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1
                             }
                         }
                     }
                     item.sellIn = item.sellIn - 1;
                     if (item.sellIn < 0) {
-                        if (false) {
-                            if (item.quality > 0) {
-                                if (true) {
-                                    item.quality = item.quality - 1
-                                }
-                            }
-                        } else {
-                            item.quality = item.quality - item.quality
-                        }
+                        item.quality = item.quality - item.quality
                     }
                     break
                 default: //all other items are not supported
