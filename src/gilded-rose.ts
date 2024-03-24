ed24aea3f6bce79802f1c5d7aac3b5abe7a97b8c
Christopher Pepper <christopher.pepper@codecomputerlove.com>
2024-03-24 22:59:38 +0000

    Refactor: extracting remaining cases and removing default

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -42,9 +42,44 @@ export class GildedRose {
                     break;
              
                 case "Sulfuras, Hand of Ragnaros":
+                    if (true) {
+                        if (item.quality > 0) {
+                            if (item.name != 'Sulfuras, Hand of Ragnaros') {
+                                item.quality = item.quality - 1
+                            }
+                        }
+                    } else {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                            if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+                                if (item.sellIn < 11) {
+                                    if (item.quality < 50) {
+                                        item.quality = item.quality + 1
+                                    }
+                                }
+                                if (item.sellIn < 6) {
+                                    if (item.quality < 50) {
+                                        item.quality = item.quality + 1
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (item.name != 'Sulfuras, Hand of Ragnaros') {
+                        item.sellIn = item.sellIn - 1;
+                    }
+                    if (item.sellIn < 0) {
+                        if (true) {
+                            if (item.quality > 0) {
+                                if (item.name != 'Sulfuras, Hand of Ragnaros') {
+                                    item.quality = item.quality - 1
+                                }
+                            }
+                        } else {
+                            item.quality = item.quality - item.quality
+                        }
+                    }
                 case "Backstage passes to a TAFKAL80ETC concert":
-                
-                default: //all other items
                     if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if (item.quality > 0) {
                             if (item.name != 'Sulfuras, Hand of Ragnaros') {
@@ -82,7 +117,10 @@ export class GildedRose {
                             item.quality = item.quality - item.quality
                         }
                     }
-                    break;
+                    break
+                default: //all other items are not supported
+                    throw new Error("Unsupported item")
+                    
             }
         });
 
